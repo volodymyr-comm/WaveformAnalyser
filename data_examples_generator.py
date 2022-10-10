164e1c1f4f53c25e6f8df2d7e0ee568436a9e005
Volodymyr <volodymyr.comm@gmail.com>
2022-10-10 17:17:36 +0200

    [DEG] Breaking to reusable functions

--- data_examples_generator.py
+++ data_examples_generator.py
@@ -13,8 +13,9 @@ def round2multiple(number, multiple):
     return round(multiple * round(number / multiple), 5)  # pay attention to round to 5 places after decimal!
 
 
-if __name__ == '__main__':
-    CYCLES: int = 20
+def generate_data_examples_dict(cycles: int) -> dict:
+    CYCLES: int = cycles
+    COMPRESSED_OUTPUT_DATA: bool = False
     TIME_DISPERSION = -0.01, 0.01
     SAMPLE_RATE = 0.01
     TRIGGER_ON = 5
@@ -30,14 +31,17 @@ if __name__ == '__main__':
         },
         'D3': {
             'Time dispersion': (-0.03, 0.03),
-        }
+        },
+        'D4': {
+            'Time dispersion': (-0.01, 0.01),
+        },
     }
 
     # create destination folder if not exists
     os.system('mkdir -p data_examples')
 
     # create digital sequence
-    morse_message = encrypt('EXAMPLE WAVEFORM ' * 5)
+    morse_message = encrypt('EXAMPLE WAVEFORM ' * 1)
     digital_sequence = morse_message.replace('.', '00').replace('-', '11').replace(' ', '')
     digital_sequence_array = np.fromstring(' '.join(digital_sequence), dtype=c_uint8, sep=' ')
 
@@ -48,7 +52,6 @@ if __name__ == '__main__':
 
     # create dummy registrations with randomized time dispersion
     cycles_dict: Dict[int, pd.DataFrame] = {}
-    ax: plt.plot = None
     for meas_cycle in range(1, CYCLES + 1):
         cycle_data_dfs_list = list()
         for ch, var in in_var_dict.items():
@@ -59,21 +62,39 @@ if __name__ == '__main__':
         cycles_dict[meas_cycle] = pd.concat(cycle_data_dfs_list)
         cycles_dict[meas_cycle] = cycles_dict[meas_cycle].reindex(
             np.append(
-                np.arange(round2multiple(cycles_dict[meas_cycle].index.min(), SAMPLE_RATE),
+                cycles_dict[meas_cycle].index.values,
+                np.arange(round2multiple(cycles_dict[meas_cycle].index.min() - 2 * SAMPLE_RATE, SAMPLE_RATE),
                           round2multiple(cycles_dict[meas_cycle].index.max() + SAMPLE_RATE, SAMPLE_RATE),
-                          SAMPLE_RATE),
-                cycles_dict[meas_cycle].index.values
+                          SAMPLE_RATE)
             )
         )\
             .sort_index()\
             .fillna(method='ffill')\
             .fillna(method='bfill')
-        cycles_dict[meas_cycle].to_csv(f'data_examples/process_{meas_cycle:05d}.csv')
-        print(cycles_dict[meas_cycle])
+        if COMPRESSED_OUTPUT_DATA:
+            # todo
+            pass
+    return cycles_dict
 
-        # optionally show plots
+
+def plot_examples_dict(cycles_dict: dict):
+    ax: plt.plot = None
+    for meas_cycle_data in cycles_dict.values():
         if ax is None:
-            ax = cycles_dict[meas_cycle].plot(legend=False)
+            ax = meas_cycle_data.plot(legend=False)
         else:
-            cycles_dict[meas_cycle].plot(ax=ax, legend=False)
+            meas_cycle_data.plot(ax=ax, legend=False)
     plt.show()
+
+
+def export_examples_dict(cycles_dict: dict):
+    for meas_cycle, meas_cycle_data in cycles_dict.items():
+        cycles_dict[meas_cycle].to_csv(f'data_examples/process_{meas_cycle:05d}.csv')
+
+
+if __name__ == '__main__':
+    cycles_dict = generate_data_examples_dict(100)
+    plot_examples_dict(cycles_dict)
+    # export_examples_dict(cycles_dict)
+
+
